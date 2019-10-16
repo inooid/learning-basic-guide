fa10a280cba572b1775acd695dab4f05dab21e2f
Inooid <justdrawcats@gmail.com>
2019-10-17 00:23:55 +0200

    Add form example

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,5 @@
 import Browser
-import Html exposing (Html, button, div, text, input)
+import Html exposing (Html, button, div, text, input, label)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
@@ -7,24 +7,60 @@ main =
   Browser.sandbox { init = init, update = update, view = view }
 
 -- Model
-type alias State = String
+type alias State =
+  {
+    name : String
+  , password : String
+  , passwordConfirmation : String
+  }
 
 init : State
-init = ""
+init =
+  State "" "" ""
 
 -- Update
-type Action = UpdateInput String
+type Action
+  = UpdateName String
+  | UpdatePassword String
+  | UpdatePasswordConfirmation String
 
 update : Action -> State -> State
-update action model =
+update action state =
   case action of
-    UpdateInput str -> str
+    UpdateName name ->
+      { state | name = name }
+    UpdatePassword password ->
+      { state | password = password }
+    UpdatePasswordConfirmation passwordConfirmation ->
+      { state | passwordConfirmation = passwordConfirmation }
 
 -- View
 view : State -> Html Action
-view model =
-  div [attribute "bob" "ross"]
-    [
-      input [placeholder "Add your awesome text hereee!! WOOO", value model, onInput UpdateInput] []
-    , div [] [ text model ]
+view state =
+  div []
+    [ div [] [viewInput "text" "Name" state.name UpdateName]
+    , div [] [viewInput "password" "Password" state.password UpdatePassword]
+    , div [] [viewInput "password" "Password confirmation" state.passwordConfirmation UpdatePasswordConfirmation]
+    , div [style "paddingTop" "16px"]
+      [ text "Is the form valid? "
+      , viewValidator state
+      ]
     ]
+
+viewInput : String -> String -> String -> (String -> Action) -> Html Action
+viewInput inputType labelText val updater =
+  label []
+  [ text (labelText ++ ":")
+  , input [type_ inputType, placeholder ("Enter your " ++ String.toLower labelText ++ "..."), value val, onInput updater] []
+  ]
+
+viewValidator : State -> Html Action
+viewValidator state =
+    if List.any String.isEmpty [state.name, state.password] then
+      div [] [text "Not all required fields are filled in"]
+
+    else if state.password == state.passwordConfirmation then
+      div [ style "color" "green" ] [ text "OK" ]
+
+    else
+      div [ style "color" "red" ] [ text "Passwords do not match!" ]
